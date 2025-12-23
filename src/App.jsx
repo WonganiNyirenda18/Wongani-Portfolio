6656bdbbd3f23c3585c0f763243fff3558fb72c8
wongani18 <wonganinyirenda18@gmail.com>
2025-12-23 13:08:03 +0200

    feat: Add initial application structure and Home component with personal introduction.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Components/Header'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
 
     </>
   )
