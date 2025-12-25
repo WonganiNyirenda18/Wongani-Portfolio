e7179098b169bdcb03ce4baf254ed6ade8d26e8e
wongani18 <wonganinyirenda18@gmail.com>
2025-12-26 00:03:24 +0200

    Added About component

---
+++ src/Components/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+    return (
+        <section>
+            <div>
+                <div>
+                    <div>
+                        <h2>About Me</h2>
+                    </div>
+
+                    <div>
+                        <p>
+                            A Third year Student at the University of Malawi (UNIMA) persuing bachelor of science in Information Systems.
+                            A founder of Tech Addicts, a small tech company that specialises in the dynamism of tech transformation from software through to hardware
+                        </p>
+                        <p>
+                            A Third year Student at the University of Malawi (UNIMA) persuing bachelor of science in Information Systems.
+                            A founder of Tech Addicts, a small tech company that specialises in the dynamism of tech transformation from software through to hardware
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
