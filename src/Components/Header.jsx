3bbab30d74e5017a041174861f970929e2ba6829
wongani18 <wonganinyirenda18@gmail.com>
2025-12-25 22:52:14 +0200

    styled header features

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,30 @@
+import Logo from '../Assets/logo.png';
+
 export default function Header() {
     return (
-        <header>
-            <div>Logo</div>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Services</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-            <button>Let's Talk </button>
+        <header className="bg-white py-4 shadow-sm">
+            <div className="max-w-xl mx-auto px-6 flex items-center justify-between">
+                <div className="flex items-center">
+                    <div className="flex items-center gap-2">
+                        <img
+                            src={Logo}
+                            alt="Logo"
+                            className="w-30 h-30 object-contain"
+                        />
+                    </div>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Projects</li>
+                        <li>Services</li>
+                    </ul>
+                </nav>
+                <button>Let's Talk </button>
+            </div>
+
         </header>
     );
 }
