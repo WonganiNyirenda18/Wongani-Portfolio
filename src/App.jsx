c1abb82207c62fb536f969331e27cae01ce98a24
wongani18 <wonganinyirenda18@gmail.com>
2025-12-25 22:12:52 +0200

    feat: Initialize React portfolio application with core components, image assets, and Tailwind CSS styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Header from './Components/Header'
 import Home from './Components/Home'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import Services from './Components/Services'
+import Contact from './Components/Contact'
 
 function App() {
 
   return (
-    <>
+    <div className="antialiased text-gray-800">
       <Header />
-      <Home />
-
-    </>
+      <main>
+        <Home />
+        <About />
+        <Projects />
+        <Services />
+        <Contact />
+      </main>
+    </div>
   )
 }
 
