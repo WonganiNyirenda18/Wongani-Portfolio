24d5a818c0a7f46f70aa10e6d086d8ef07b2bb67
wongani18 <wonganinyirenda18@gmail.com>
2025-12-25 22:23:02 +0200

    feat: Add basic Header component with navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import Header from './Components/Header'
-import Home from './Components/Home'
-import About from './Components/About'
-import Projects from './Components/Projects'
-import Services from './Components/Services'
-import Contact from './Components/Contact'
+
 
 function App() {
 
   return (
     <div className="antialiased text-gray-800">
       <Header />
-      <main>
-        <Home />
-        <About />
-        <Projects />
-        <Services />
-        <Contact />
-      </main>
+
     </div>
   )
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return (
+        <header>
+            <div>Logo</div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <button>Let's Talk </button>
+        </header>
+    );
+}
