079d94befa056228398bef7b58069553defa9ef8
wongani18 <wonganinyirenda18@gmail.com>
2025-12-26 12:38:04 +0200

    feat: add Header component with logo, navigation, and call-to-action button.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import Logo from '../Assets/logo.png'
+
+export default function Header() {
+    return (
+        <header className="bg-white py-4 shadow-sm">
+            <div className="max-w-7xl mx-auto px-6 flex items-center justify-between">
+                <div className="flex items-center">
+
+                    <div className="flex items-center gap-2">
+                        <img
+                            src={Logo}
+                            alt="Logo"
+                            className="w-30 h-30 object-contain"
+                        />
+
+                    </div>
+                </div>
+
+                <nav className="hidden md:block">
+                    <ul className="flex items-center gap-8 font-medium text-gray-700">
+                        <li className="cursor-pointer hover:text-blue-700 transition">Home</li>
+                        <li className="cursor-pointer hover:text-blue-600 transition">About</li>
+                        <li className="cursor-pointer hover:text-blue-600 transition">Projects</li>
+                        <li className="cursor-pointer hover:text-blue-600 transition">Services</li>
+                    </ul>
+                </nav>
+
+                <div>
+                    <button
+                        type="button"
+                        className="bg-blue-600 text-white px-6 py-2 rounded-md font-medium hover:bg-blue-700 transition shadow-lg shadow-blue-200 cursor-pointer"
+                    >
+                        Let's Talk
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}
