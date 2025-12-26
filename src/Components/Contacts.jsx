860e794ef9839778e53464c257f0a0397d7338b4
wongani18 <wonganinyirenda18@gmail.com>
2025-12-26 10:56:30 +0200

    added a form for reach

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -6,6 +6,21 @@ export default function Contacts() {
                 <div className="flex flex-col items-center mb-12">
                     <h2 className="text-3xl font-bold text-gray-900 mb-2">Contacts</h2>
                 </div>
+
+                <div>
+                    <form>
+                        <div className="mb-6">
+                            <input type="text" placeholder="Name" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                        </div>
+                        <div className="mb-6">
+                            <input type="email" placeholder="Email" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                        </div>
+                        <div className="mb-6">
+                            <textarea placeholder="Message" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+                        </div>
+                        <button type="submit" className="bg-blue-600 text-white px-6 py-2 rounded-md font-medium hover:bg-blue-700 transition shadow-lg shadow-blue-200 cursor-pointer">Send Message</button>
+                    </form>
+                </div>
             </div>
         </section>
     );
