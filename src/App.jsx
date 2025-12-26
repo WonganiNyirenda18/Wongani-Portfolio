da7c98492ec63dab981462ba34aeef6ed1674fa5
wongani18 <wonganinyirenda18@gmail.com>
2025-12-26 10:37:09 +0200

    feat: Initialize portfolio application structure and add Contacts component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Components/Home'
 import About from './Components/About'
 import Projects from './Components/Projects'
 import Services from './Components/Services'
+import Contacts from './Components/Contacts'
 
 
 function App() {
