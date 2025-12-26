a48d534d08faa17900a040ae573c4ae9606ddf0f
wongani18 <wonganinyirenda18@gmail.com>
2025-12-26 10:54:33 +0200

    feat: add Contacts component and integrate it into the main application layout.

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -1,7 +1,12 @@
 export default function Contacts() {
     return (
-        <section>
+        <section className="bg-white-50 py-20" id="contacts">
+            <div className="max-w-xl mx-auto px-6 text-center">
 
+                <div className="flex flex-col items-center mb-12">
+                    <h2 className="text-3xl font-bold text-gray-900 mb-2">Contacts</h2>
+                </div>
+            </div>
         </section>
     );
 }
