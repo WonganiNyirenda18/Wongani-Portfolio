91cf77e4d674aac7e805e120c776dfec2401d0c7
wongani18 <wonganinyirenda18@gmail.com>
2026-02-02 11:36:47 +0200

    feat: modified the transition elements so its visible enough on small screen

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -31,11 +31,14 @@ export default function Projects() {
                     {projects.map((project, index) => (
                         <motion.div
                             key={index}
-                            initial={{ opacity: 0, x: 50 }}
+                            initial={{ opacity: 0, x: -120 }}
                             whileInView={{ opacity: 1, x: 0 }}
-                            viewport={{ once: true }}
-                            transition={{ duration: 0.6, delay: index * 0.15 }}
-                            className="bg-blue-50/50 rounded-lg overflow-hidden hover:shadow-lg transition duration-300 border border-gray-100">
+                            viewport={{ amount: 0.3 }}
+                            transition={{
+                                duration: 0.8,
+                                ease: "easeOut"
+                            }}
+                            className="bg-blue-50/50 rounded-lg overflow-hidden hover:shadow-lg transition duration-300 border border-gray-100" >
                             <div className="h-48 overflow-hidden">
                                 <img
                                     src={project.image}
@@ -60,7 +63,7 @@ export default function Projects() {
                     </button>
                 </div>
             </div>
-        </section>
+        </section >
 
     );
 }
